785d8a6b640b583c90c4d549248673c258599cdd
Vladyslav <vitbuianov.20130225@gmail.com>
2026-07-25 19:02:32 +0200

    first commit

---
+++ src/js/data.js
@@ -0,0 +1,24 @@
+export const games = {
+    title: "Best games of 2026",
+  items: [
+    "EA FC26",
+    "GMM",
+    "Minecraft",
+    "Roblox",
+    "Brawl Stars",
+    "KC GO",
+    "Forza Horizon 6",
+    "Rust",
+  ],
+};
+
+export const gamesList = [
+  "EA FC26",
+  "GMM",
+  "minecraft",
+  "roblox",
+  "Brawl Stars",
+  "KC GO",
+  "Forza Horizon 6",
+  "RUST",
+];
---
+++ src/js/hadlebars.js
@@ -0,0 +1,12 @@
+import Handlebars from "handlebars";
+// import gamesListTemplate from "bundle-text:../templates/game-list.hbs";
+import { games, gamesList } from "./data";
+import gamesTemplate from "bundle-text:../templates/games.hbs";
+
+const gameContainerRef = document.querySelector("[data-game-container]")
+// const createGamesTemplate = Handlebars.compile(gamesListTemplate);
+// const gamesMarkup = createGamesTemplate(games);
+// gameContainerRef.innerHTML = gamesMarkup;
+const gamesArrTemplate = Handlebars.compile(gamesTemplate);
+const gamesArrMarkup = gamesArrTemplate(gamesList);
+gameContainerRef.insertAdjacentHTML("afterbegin", gamesArrMarkup)
