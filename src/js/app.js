fe23e8915fa598874dc7c41fc05cee0db6a90733
Vladyslav <vitbuianov.20130225@gmail.com>
2026-08-01 17:39:30 +0200

    new

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
-import "./hadlebars";
+// import "./hadlebars";
+import Handlebars from "handlebars";
+import users from "../users.json";
+import usersTemplate from "../templates/users.hbs";
+
+const userBoxRef = document.querySelector(".users__box");
+
+userBoxRef.innerHTML = usersTemplate(users);
